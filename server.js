c3faa7939639f99b8513953abed905df50d2bc12
Scott O'Malley <scottyomalley@gmail.com>
2014-12-03 00:31:19 +0000

    New Architecture changes

---
+++ server.js
@@ -0,0 +1,55 @@
+
+var fortune = require('fortune')
+  , express = fortune.express
+  , path = require("path");
+
+
+var container = express()
+  , port = 3000;
+
+
+
+
+
+var rankingsAPI = fortune({
+  db: 'rankings',
+  path: './data/'
+})
+.resource('fencer', {
+    firstname: String,
+    lastname: String,
+    gender: String,
+    club: String,
+    results: ['result']
+    
+    
+}).resource('competition', {
+    name: String,
+    shortName: String,
+    results: ['result']
+})
+.resource('result', {
+       
+        
+        placing: Number,
+        points: Number,
+        competition: 'competition'
+});
+
+
+container
+  .use(rankingsAPI.router)
+  .use(express.static(path.join(__dirname, 'app')))
+  .listen(port);
+// var app = express();
+
+// app.configure(function () {
+//   app.set('port', process.env.PORT || 3000);
+//   app.use(express.logger('dev'));
+//   app.use(express.bodyParser()),
+//   app.use(express.static(path.join(__dirname, 'app')));
+// });
+
+
+
+console.log('Server listening on port ' + container.get('port'));
