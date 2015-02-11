d053b57fabea5b6115bb88d1cbbeffd15dc28496
Scott O'Malley <scottyomalley@gmail.com>
2015-02-11 01:53:26 +0000

    sync

--- app/js/controllers/competitionCtrl.js
+++ app/js/controllers/competitionCtrl.js
@@ -1,58 +1,17 @@
 'use strict';
 
 angular.module('RankingsApp')
-    .controller('CompetitionController', function ($scope, Restangular, $routeParams, $location, ResultsCalculator, CategoriesService) {
+    .controller('CompetitionController', function($scope, Restangular, $routeParams, $location, ResultsCalculator, CategoriesService) {
 
-        $scope.form = {};
         $scope.results = [];
         $scope.fencers = [];
+        $scope.isFocused = false;
 
-        Restangular.one('competitions', $routeParams.competitionID).get().then(function (competition) {
-            $scope.competition = competition;
+        $scope.onBlur = function(){
+            $scope.isFocused = false;
+        }
 
-            $scope.competition.getList('instances').then(function (instances) {
-                $scope.instances = instances;
-                if ($routeParams.instanceID) {
-                    $scope.selectedInstance = _.find($scope.instances, function (instance) {
-                        return instance.id === $routeParams.instanceID
-                    });
-
-                    $scope.selectedInstance.getList('results').then(function (results) {
-                        $scope.results = results;
-                    });
-                }
-            });
-
-
-        });
-
-        Restangular.all('fencers').getList().then(function (fencers) {
-            $scope.fencers = fencers;
-        });
-
-
-        $scope.addResult = function (form) {
-            var result = Restangular.one('results');
-            result.fencer = form.selectedFencer.id;
-            result.competition = $routeParams.competitionID;
-            result.instance = $routeParams.instanceID;
-            result.placing = $scope.getNextPlacing();
-            result.points = $scope.getPointsForPlacing(result.placing);
-            Restangular.service('results').post(result).then(function (response) {
-                $scope.results.push(response);
-            });
-
-            $scope.form = {};
-
-        };
-
-        $scope.$watch('selectedInstance', function () {
-            if ($scope.selectedInstance) {
-                $location.path('/competition/' + $scope.competition.id + '/instance/' + $scope.selectedInstance.id);
-            }
-        });
-
-        $scope.getNextPlacing = function () {
+        $scope.getNextPlacing = function() {
 
             var x = $scope.results.length;
 
@@ -63,11 +22,71 @@ angular.module('RankingsApp')
             }
         };
 
-        $scope.maximumNumberOfResultsReached = function () {
+
+
+        Restangular.one('competitions', $routeParams.competitionID).get().then(function(competition) {
+            $scope.competition = competition;
+
+            $scope.competition.getList('instances').then(function(instances) {
+                $scope.instances = instances;
+                if ($routeParams.instanceID) {
+                    $scope.selectedInstance = _.find($scope.instances, function(instance) {
+                        return instance.id === $routeParams.instanceID
+                    });
+
+                    $scope.selectedInstance.getList('results').then(function(results) {
+                        $scope.results = results;
+
+                        $scope.form = {
+                            placing: $scope.getNextPlacing()
+                        };
+                    });
+                }
+            });
+
+
+        });
+
+        Restangular.all('fencers').getList().then(function(fencers) {
+            $scope.fencers = fencers;
+        });
+
+
+        $scope.addResult = function(form) {
+
+            //TODO: some kind of form validation
+
+            var result = Restangular.one('results');
+            result.fencer = form.selectedFencer.id;
+            result.competition = $routeParams.competitionID;
+            result.instance = $routeParams.instanceID;
+            result.placing = $scope.getNextPlacing();
+            result.points = $scope.getPointsForPlacing(result.placing);
+            result.excludedFromRankings = form.excludedFromRankings;
+
+            Restangular.service('results').post(result).then(function(response) {
+                $scope.results.push(response);
+                $scope.form = {
+                    placing: $scope.getNextPlacing()
+                };
+
+                $scope.isFocused = true;
+            });
+        };
+
+        $scope.$watch('selectedInstance', function() {
+            if ($scope.selectedInstance) {
+                $location.path('/competition/' + $scope.competition.id + '/instance/' + $scope.selectedInstance.id);
+            }
+        });
+
+
+
+        $scope.maximumNumberOfResultsReached = function() {
             if ($scope.selectedInstance) {
                 var competitorCategories = CategoriesService.numberOfCompetitorsCategories();
 
-                var category = _.find(competitorCategories, function (x) {
+                var category = _.find(competitorCategories, function(x) {
                     return x.code === $scope.selectedInstance.numberOfCompetitors;
                 });
 
@@ -81,11 +100,11 @@ angular.module('RankingsApp')
             return false;
         }
 
-        $scope.getPointsForPlacing = function (placing) {
+        $scope.getPointsForPlacing = function(placing) {
             return ResultsCalculator.calculatePoints(placing, $scope.selectedInstance);
         }
 
-        $scope.log = function (result) {
+        $scope.log = function(result) {
             console.log(result)
         }
 
