a839e891dc7ef9ea13471c82655cb1f17f23ba82
Scott O'Malley <scottyomalley@gmail.com>
2015-02-15 18:20:12 +0000

    Ranking Engine now calculates rankings

--- app/js/controllers/rankingCtrl.js
+++ app/js/controllers/rankingCtrl.js
@@ -5,7 +5,7 @@ angular.module('RankingsApp')
 
 
 
-        var defer = $q.defer();
+
         var promises = [];
 
         $scope.rankings = [];
@@ -19,14 +19,73 @@ angular.module('RankingsApp')
             var points = 0;
 
             for (var i = 0; i < 6; i++) {
-                if(sorted[i])
-                {
+                if (sorted[i]) {
                     points += sorted[i].points;
                 }
             }
 
             return points;
         }
+
+
+        function createRanking(fencer, results, instances) {
+            var results = _.filter(results, function(result) {
+                return instances[result.links.instance];
+            });
+
+            var ranking = {
+                fencer: fencer.fullName(),
+                points: calculatePoints(results)
+            };
+
+            _.each(results, function(i) {
+                ranking[i.links.instance] = i.points;
+            });
+
+            return ranking;
+        }
+
+
+
+        function calculateRanking(fencer, previousFencer, numSamePoints) 
+        {
+
+            if (fencer.points == previousFencer.points) {
+                fencer.rank = previousFencer.rank;
+                numSamePoints++;
+            } 
+            else 
+            {
+                fencer.rank = previousFencer.rank + numSamePoints;
+                numSamePoints = 1;
+            }
+
+            return numSamePoints;
+        }
+
+
+        function createRankingsFromResults(instances, fencers, resultsList) {
+            var rankings = []
+
+            for (var i = 0; i < fencers.length; i++) {
+                rankings.push(createRanking(fencers[i], resultsList[i], instances));
+            }
+
+            rankings = _.sortBy(rankings, function(rank) {
+                return rank.points
+            }).reverse();
+
+            rankings[0].rank = 1;
+            var numSamePoints = 1;
+
+            for (var i = 1; i < rankings.length; i++) {
+                numSamePoints = calculateRanking(rankings[i], rankings[i - 1], numSamePoints);
+            }
+
+            return rankings;
+        }
+
+
 
         function getLatestInstance(promiseResults) {
 
@@ -48,34 +107,26 @@ angular.module('RankingsApp')
             });
 
             Restangular.all('fencers').getList().then(function(fencers) {
-                $scope.fencers = fencers;
 
-                _.each($scope.fencers, function(fencer) {
-                    fencer.getList('results').then(function(response) {
+                var rankings = [];
+                var fencerResultsPromises = [];
 
+                _.each(fencers, function(fencer) {
+                    fencerResultsPromises.push(fencer.getList('results'));
+                })
 
-
-                        var results = _.filter(response, function(result) {
-                            return instances[result.links.instance];
-                        });
-
-                        var ranking = {
-                            fencer: fencer.fullName(),
-                            points: calculatePoints(results)
-                        };
-
-                        _.each(results, function(i){
-                            ranking[i.links.instance] = i.points;
-                        });
-
-                        $scope.rankings.push(ranking);
-
-                    })
+                var all = $q.all(fencerResultsPromises);
+                all.then(function(response) {
+                    $scope.rankings = createRankingsFromResults(instances, fencers, response)
+                    $scope.rankingsComplete = true;
                 });
 
-                $scope.rankingsComplete = true;
             });
         };
+
+
+
+
 
         Restangular.all('competitions').getList().then(function(competitions) {
             $scope.competitions = competitions;
@@ -88,23 +139,22 @@ angular.module('RankingsApp')
 
             var all = $q.all(promises);
             all.then(getLatestInstance)
-
+                .then(function(rankings) {
+                    $scope.rankings = rankings;
+                })
 
         });
 
 
-        $scope.getPointsFromResultAtCompetition = function(fencer, comp){
-            if(fencer.Results())
-            {
+        $scope.getPointsFromResultAtCompetition = function(fencer, comp) {
+            if (fencer.Results()) {
                 var results = fencer.Results();
                 var points = results[comp.Instance.id].points;
                 return points;
-            }
-            else
-            {
+            } else {
                 return "-"
-            }       
-             
+            }
+
         }
 
 
